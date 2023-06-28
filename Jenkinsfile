a497f5ab76e6265d193cecfd2d3dceaf62e24eaf
EkoAgustina <eko.agustina86@gmail.com>
2023-06-28 16:10:05 +0700

    Update browser config

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,25 +1,37 @@
 pipeline{
     agent any
+    tools {nodejs "node18.6.0"}
     parameters{
         string(name: "Repositories", defaultValue: "", trim: true, description: "Please enter your Repositories")
-        choice(name: "Browser", choices: ["chrome", "MicrosoftEdge"], description: "Please select a browser")
+        choice(name: "Browser", choices: ["headless", "chrome"], description: "Please select a browser")
         string(name: "Tags", defaultValue: "@", trim: true, description: "Please enter the desired tags")
     }
     stages{
-        stage('Build'){
+        stage('Project preparation'){
             steps{
                 git "$params.Repositories"
-                echo '----->Install a package and it\'s dependencies<-----'
+                echo '------------------------>Install a package and it\'s dependencies<-----------------------'
                 script{
-                    bat "npm install"
+                    sh 'npm install'
+                    sleep(time: 1, unit: "SECONDS")
                 }
             }
         }
-        stage('Running tests'){
+        stage('Build containers'){
             steps{
+                echo '------------------->Builds and starts containers for a service defined<-------------------'
+                script{
+                    sh 'docker-compose up -d'
+                    sleep(time: 1, unit: "SECONDS")
+                }
+            }
+        }
+        stage('Test'){
+            steps{
+                echo '------------------------------------>Running test<------------------------------------'
                 script{
                     sleep(time: 1, unit: "SECONDS")
-                    bat "npm run test -- --browserName=$Browser --cucumberTags=$Tags"
+                    sh 'npm run docker -- --cucumberTags=$Tags'
                 }
             }
         }
@@ -27,6 +39,23 @@ pipeline{
             steps{
                 allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
             }
+        }
+    }
+    post {
+        success{
+            echo '--------------------->Stops and removes containers for a service defined<---------------------'
+            script{
+                sh 'docker-compose down'
+                sleep(time: 1, unit: "SECONDS")
+            }
+        }
+        always {
+            cleanWs(cleanWhenNotBuilt: false,
+                    deleteDirs: true,
+                    disableDeferredWipeout: true,
+                    notFailBuild: true,
+                    patterns: [[pattern: '.gitignore', type: 'INCLUDE'],
+                               [pattern: '.propsfile', type: 'EXCLUDE']])
         }
     }
 }
--- config/browser.conf.js
+++ config/browser.conf.js
@@ -1,64 +1,82 @@
+const { platform  } = require('node:process');
 const {config} = require('./wdio.conf');
 const yargs = require('yargs').argv;
 let browserName = yargs.browserName;
 
 
-const drivers = {
-    chrome: { version: '114.0.5735.90' }, // https://chromedriver.chromium.org/
-    firefox: { version: '0.32.1' }, // https://github.com/mozilla/geckodriver/releases
-    chromiumedge: { version: '114.0.1823.18' } // https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+if (platform === 'linux'){
+    config.capabilities = [
+        {
+            maxInstances: 5,
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: ['--disable-gpu','--disable-dev-shm-usage']
+            },
+            acceptInsecureCerts: true
+        }
+    ]
+    config.services =[]
 }
-switch (browserName){
-    case 'headless':
-        config.capabilities = [
-            {
-                maxInstances: 5,
-                browserName: 'chrome',
-                'goog:chromeOptions': {
-                    args: ['headless', 'disable-gpu']
-                },
-                acceptInsecureCerts: true
-            }
-        ]
-        break;
-    case 'chrome':
-        config.capabilities = [
-            {
-                maxInstances: 5,
-                browserName: 'chrome',
-                acceptInsecureCerts: true
-            }
-        ]
-        break;
-    case 'firefox':
-        config.capabilities = [
-            {
-                maxInstances: 5,
-                browserName: 'firefox',
-                acceptInsecureCerts: true
-            }
-        ]
-        break;
-    case 'MicrosoftEdge':
-        config.capabilities = [
-            {
-                maxInstances: 5,
-                browserName: 'MicrosoftEdge',
-                acceptInsecureCerts: true
-            }
-        ]
-        break;
-    default:
-        throw new Error(`Condition ${browserName}, condition not recognized!`)
+else {
+    const drivers = {
+        chrome: { version: '114.0.5735.90' }, // https://chromedriver.chromium.org/
+        firefox: { version: '0.32.1' }, // https://github.com/mozilla/geckodriver/releases
+        chromiumedge: { version: '114.0.1823.18' } // https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+    }
+    switch (browserName){
+        case 'headless':
+            config.capabilities = [
+                {
+                    maxInstances: 5,
+                    browserName: 'chrome',
+                    'goog:chromeOptions': {
+                        args: ['headless', 'disable-gpu']
+                    },
+                    acceptInsecureCerts: true
+                }
+            ]
+            break;
+        case 'chrome':
+            config.capabilities = [
+                {
+                    maxInstances: 5,
+                    browserName: 'chrome',
+                    acceptInsecureCerts: true
+                }
+            ]
+            break;
+        case 'firefox':
+            config.capabilities = [
+                {
+                    maxInstances: 5,
+                    browserName: 'firefox',
+                    acceptInsecureCerts: true
+                }
+            ]
+            break;
+        case 'MicrosoftEdge':
+            config.capabilities = [
+                {
+                    maxInstances: 5,
+                    browserName: 'MicrosoftEdge',
+                    acceptInsecureCerts: true
+                }
+            ]
+            break;
+        default:
+            throw new Error(`Condition ${browserName}, condition not recognized!`)
+    }
+
+    config.services = [
+        ['selenium-standalone', {
+            logPath: 'logs',
+            installArgs: { drivers }, // drivers to install
+            args: { drivers } // drivers to use
+        }]
+    ]
 }
 
-config.services = [
-    ['selenium-standalone', {
-        logPath: 'logs',
-        installArgs: { drivers }, // drivers to install
-        args: { drivers } // drivers to use
-    }]
-]
+
 
 config.cucumberOpts.tagExpression = yargs.cucumberTags;
 
