ec932dd2ad36596af5169ffa62842f84bd7f37ba
EkoAgustina <eko.agustina86@gmail.com>
2023-10-07 18:05:59 +0700

    update

--- config/browser.conf.js
+++ config/browser.conf.js
@@ -15,14 +15,23 @@ if (globalVariables.os === 'linux') {
       },
       acceptInsecureCerts: true,
     },
-    // {
-    //   maxInstances: 5,
-    //   browserName: 'firefox',
-    //   'moz:firefoxOptions': {
-    //     args: ['-headless'],
-    //   },
-    //   acceptInsecureCerts: true,
-    // },
+    {
+      maxInstances: 5,
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['-headless'],
+      },
+      acceptInsecureCerts: true,
+    },
+    {
+      maxInstances: 5,
+      browserName: 'msedge',
+      'ms:edgeOptions': {
+        args: ['--headless'],
+      },
+      acceptInsecureCerts: true,
+    },
+
   ];
   config.services = [
     globalVariables.services,
