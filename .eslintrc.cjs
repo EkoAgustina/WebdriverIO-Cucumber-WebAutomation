7c98690503b589e0014a0d1e0cc64e5e2bcc2fc6
EkoAgustina <eko.agustina86@gmail.com>
2024-01-13 17:50:59 +0700

    Updated ESLint format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,44 @@
 module.exports = {
-    "globals": {
-        "$": true,
-        "browser": true
+    globals: {
+      $: true,
+      browser: true,
+    },
+    env: {
+      es2021: true,
+      node: true,
+      browser: true,
+      mocha: true,
+    },
+    extends: 'eslint:recommended',
+    plugins: [
+      'standard',
+      'promise',
+      'webdriverio',
+      'cucumber',
+    ],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+        ],
+        parserOptions: {
+          sourceType: "script",
+        },
       },
-    "env": {
-        "es2021": true,
-        "node": true,
-        "browser": true,
-        "mocha": true
-    },
-    "extends": "eslint:recommended",
-    "plugins": [
-        'standard',
-        'promise',
-        'webdriverio',
-        'cucumber'
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    "rules": {
-        'no-useless-escape': 'off',
-        'no-async-promise-executor': 'off',
+    rules: {
+      'no-useless-escape': 'off',
+      'no-async-promise-executor': 'off',
+      
     },
-    "ignorePatterns": ['reporter/']
-}
+    ignorePatterns: ['reporter/'],
+  };
+  
