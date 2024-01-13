c87e72444f2867aaacf5b820c78dde93b37c467f
EkoAgustina <eko.agustina86@gmail.com>
2024-01-13 21:15:54 +0700

    Change the local test runner configuration

--- runner.sh
+++ runner.sh
@@ -1,6 +1,21 @@
 hostName='localhost:8080'
-browserName='headless'
+browserName='chrome'
 cucumberTags='@LoginSuccessfully'
+
+if [ -z "$hostName" ]; then
+  echo "Error: hostName cannot be empty!"
+  exit 1
+fi
+
+if [ -z "$browserName" ]; then
+  echo "Error: browserName cannot be empty!"
+  exit 1
+fi
+
+if [ -z "$cucumberTags" ]; then
+  echo "Error: cucumberTags cannot be empty!"
+  exit 1
+fi
 
 export cucumberTagExpression=${cucumberTags}
 export hostName=${hostName}
